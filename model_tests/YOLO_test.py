f91b6412f4f17f60f8e5270f08461e2ed2f91924
Benjamin Lambright <bwlambright@gmail.com>
2025-01-08 11:40:43 -0500

    Delete model_tests directory

--- model_tests/YOLO_test.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
-
-# Train the model
-results = model.train(data='dataset/test_images', epochs=3, imgsz=640)
